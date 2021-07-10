7bd6ae7fa1fe8714d0686728bf255e2a1992a4d2
Saull Brandão <saull@outlook.com>
2021-07-10 10:33:57 -0300

    test: user can login

---
+++ cypress/e2e/login.js
@@ -0,0 +1,17 @@
+describe('login', () => {
+  it('should login a existing user', () => {
+    cy.createUser().then(user => {
+      cy.visit('/')
+      cy.findByText(/login/i).click()
+      cy.findByLabelText(/username/i).type(user.username)
+      cy.findByLabelText(/password/i).type(user.password)
+      cy.findByText(/submit/i).click()
+
+      cy.url().should('eq', `${Cypress.config().baseUrl}/`)
+      cy.window()
+        .its('localStorage.token')
+        .should('be.a', 'string')
+      cy.findByTestId('username-display').should('have.text', user.username)
+    })
+  })
+})
